4d2bb1c93c46a1e2b494c6547b525b19e3c39b41
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-06-17 03:55:41 +0300

    commit for start of OOP section

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const onat = {
+  id: 2207080990,
+  name: `Onat`,
+  surname: `Arslan`,
+  friends: [`Rugi`, `Coni`, `Cano`, `Ado`],
+};
+
+console.log(onat.id);
+console.log(onat.name);
+console.log(onat.surname);
+
+onat.friends.forEach((el, i, arr) => {
+  console.log(`Friend name is : ${el} and index of ${el} is ${i}`);
+});
+
+console.log(
+  onat.friends.reduce((acc, el, i, arr) => {
+    return acc + el;
+  }, `This function will concat all names`)
+);
